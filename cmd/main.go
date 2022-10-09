ae0a72f3db2c178c3fc0a578968048150f5e5b1c
realVeer <realVeer@qkbyte.com>
2022-10-09 12:39:34 +0800

    暂存

--- cmd/main.go
+++ cmd/main.go
@@ -23,7 +23,6 @@ import (
 	"os"
 	"path/filepath"
 	"runtime"
-	"runtime/debug"
 	"sort"
 	"strings"
 
@@ -32,7 +31,6 @@ import (
 	"github.com/minio/pkg/trie"
 	"github.com/minio/pkg/words"
 	"github.com/qkbyte/minio/internal/color"
-	"github.com/qkbyte/minio/internal/logger"
 )
 
 // GlobalFlags - global flags for minio.
@@ -190,26 +188,6 @@ func printMinIOVersion(c *cli.Context) {
 func Main(args []string) {
 	// Set the minio app name.
 	appName := filepath.Base(args[0])
-
-	if os.Getenv("_MINIO_DEBUG_NO_EXIT") != "" {
-		freeze := func(_ int) {
-			// Infinite blocking op
-			<-make(chan struct{})
-		}
-
-		// Override the logger os.Exit()
-		logger.ExitFunc = freeze
-
-		defer func() {
-			if err := recover(); err != nil {
-				fmt.Println("panic:", err)
-				fmt.Println("")
-				fmt.Println(string(debug.Stack()))
-			}
-			freeze(-1)
-		}()
-	}
-
 	// Run the app - exit on error.
 	if err := newApp(appName).Run(args); err != nil {
 		os.Exit(1) //nolint:gocritic
